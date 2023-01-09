a35024f4caee62811fded97e1d32ef9fe7107e8a
marcus featherston <marcusovfeatherston@gmail.com>
2023-01-09 11:55:31 -0700

    Strings labcompletion

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code in this file!
+const currentUser = "Marcus";
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser[0]}!`;
+
+
+
+
